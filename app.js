73890c3a179da48d991ea000194a5d677cf793d5
Kouji Anzai <anzai.k.aa@gmail.com>
2012-02-01 11:58:10 +0900

    test

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var fcgiApp = require("./fcgi"),
-	http = require("http");
-
+var fcgiApp = require("./fcgi"), http = require("http");
 
 var myServer = http.createServer(function(req, res) {
-	setTimeout(function() {
-		res.writeHead(200, {"Content-type": "text/html"});
-		res.write("It works! by kanzmrsw !" + Date.now());
-		res.end();
-		console.log("Wrote response.");
-	}, 1000);
-	//throw new Error("Bollocks.");
+    setTimeout(function() {
+        res.writeHead(200, {
+            "Content-type" : "text/html"
+        });
+        res.write("It works! by kanzmrsw !" + Date.now());
+        res.write("It works! by kanzmrsw !" + Date.now());
+        res.end();
+        console.log("Wrote response.");
+    }, 1000);
+    //throw new Error("Bollocks.");
 });
-
 // Instead of this:
 //myServer.listen(12345);
 
